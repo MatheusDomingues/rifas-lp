b08a724a21e599df620be65a0b6a1591f18a8b0b
Matheus Domingues <matheus_luige@hotmail.com>
2024-07-12 18:04:05 -0300

    feat: create form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,40 @@
 import { Suspense } from 'react'
 
-import { LandingPage } from '@/components/landing-page'
+import { Form } from '@/components/form'
+import { Header } from '@/components/header'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 export default function Page() {
   return (
     <Suspense fallback={<p>Carregando...</p>}>
-      <LandingPage />
+      <main className='relative w-screen h-screen bg-lp bg-center bg-no-repeat bg-fixed bg-cover text-white scroll-smooth'>
+        <Header />
+
+        <ScrollArea className='flex w-full h-full'>
+          <div className='flex flex-col gap-6 items-center justify-start pb-10 pt-20'>
+            <div
+              id='inicio'
+              className='flex p-4 max-h-[70%] justify-center w-full'
+            >
+              <img
+                alt='Compre e concorra'
+                src='/images/banner.png'
+                className='w-fit h-fit max-h-full'
+              />
+            </div>
+
+            <div id='passos' className='flex p-4 justify-center w-full'>
+              <img
+                alt='Compre e concorra'
+                src='/images/steps.png'
+                className='w-fit h-fit max-w-full'
+              />
+            </div>
+
+            <Form />
+          </div>
+        </ScrollArea>
+      </main>
     </Suspense>
   )
 }

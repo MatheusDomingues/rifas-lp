06c3c94ebe0e03a93c946b72256f628b893380d9
Matheus Domingues <matheus_luige@hotmail.com>
2024-07-22 17:14:32 -0300

    fix: img spacement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,12 +33,12 @@ export default function Page() {
           <img
             alt='Compre e concorra'
             src='/images/bee.png'
-            className='max-w-full max-h-[20%] md:max-h-[30%] object-contain absolute right-4 md:right-12 top-10'
+            className='max-w-full max-h-[20%] md:max-h-[30%] object-contain absolute right-2 md:right-12 top-14 md:top-10'
           />
           <img
             alt='Compre e concorra'
             src='/images/trial.png'
-            className='max-w-full max-h-[15%] md:max-h-[25%] object-contain absolute left-4 md:left-10 top-20'
+            className='max-w-full max-h-[15%] md:max-h-[25%] object-contain absolute left-2 md:left-10 top-[74px] md:top-20'
           />
         </ScrollArea>
       </main>

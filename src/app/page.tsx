97021d2aa67164e772ab137ebe8d139e7838a035
Matheus Domingues <matheus_luige@hotmail.com>
2024-07-24 17:34:51 -0300

    feat: add image steps mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,12 @@ export default function Page() {
               <img
                 alt='Compre e concorra'
                 src='/images/steps.png'
-                className='max-w-full max-h-full object-contain z-10'
+                className='max-w-full max-h-full object-contain z-10 hidden md:flex'
+              />
+              <img
+                alt='Compre e concorra'
+                src='/images/steps-mobile.png'
+                className='max-w-full max-h-full object-contain z-10 md:hidden'
               />
             </div>
 
@@ -33,12 +38,12 @@ export default function Page() {
           <img
             alt='Compre e concorra'
             src='/images/bee.png'
-            className='max-w-full max-h-[20%] md:max-h-[30%] object-contain absolute right-2 md:right-12 top-14 md:top-10'
+            className='max-w-full max-h-[30%] md:max-h-[40%] object-contain absolute -right-10 top-[64px]'
           />
           <img
             alt='Compre e concorra'
             src='/images/trial.png'
-            className='max-w-full max-h-[15%] md:max-h-[25%] object-contain absolute left-2 md:left-10 top-[74px] md:top-20'
+            className='max-w-full max-h-[15%] md:max-h-[20%] object-contain absolute -left-6 top-[64px]'
           />
         </ScrollArea>
       </main>
